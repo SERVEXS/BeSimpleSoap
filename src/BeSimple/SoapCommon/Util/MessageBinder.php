1959bd038def15450701706749e14fde86e73b0e
Joshua Bours (ServeXS) <joshua.bours@servexs.nl>
2022-06-22 12:09:20 +0200

    restore method propertyWrite of MessageBinder.php to original version

--- src/BeSimple/SoapCommon/Util/MessageBinder.php
+++ src/BeSimple/SoapCommon/Util/MessageBinder.php
@@ -13,6 +13,9 @@
 namespace BeSimple\SoapCommon\Util;
 
 use BeSimple\SoapCommon\PropertyAccess\ReflectionPropertyAccessor;
+use InvalidArgumentException;
+use ReflectionClass;
+use RuntimeException;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 
@@ -31,13 +34,19 @@ class MessageBinder
      */
     protected $propertyAccessor;
 
+    /**
+     * @var ReflectionClass
+     */
+    protected $reflectionClass;
+
     public function __construct($message)
     {
         if (!is_object($message)) {
-            throw new \InvalidArgumentException(sprintf('The message must be an object, %s given', gettype($message)));
+            throw new InvalidArgumentException(sprintf('The message must be an object, %s given', gettype($message)));
         }
 
         $this->message = $message;
+        $this->reflectionClass = new ReflectionClass($this->message);
         $this->propertyAccessor = new ReflectionPropertyAccessor(
             PropertyAccess::createPropertyAccessor()
         );
@@ -50,6 +59,25 @@ class MessageBinder
 
     public function writeProperty($property, $value)
     {
-        $this->propertyAccessor->setValue($this->message, $property, $value);
+        if ($this->reflectionClass->hasMethod($setter = 'set'.$property)) {
+            if (!$this->reflectionClass->getMethod($setter)->isPublic()) {
+                throw new RuntimeException(sprintf('Method "%s()" is not public in class "%s"', $setter, $this->reflectionClass->name));
+            }
+
+            $this->message->{$setter}($value);
+        } elseif ($this->reflectionClass->hasMethod('__set')) {
+            // needed to support magic method __set
+            $this->message->{$property} = $value;
+        } elseif ($this->reflectionClass->hasProperty($property)) {
+            $p = $this->reflectionClass->getProperty($property);
+            if (!$p->isPublic()) {
+                $p->setAccessible(true);
+            }
+
+            $p->setValue($this->message, $value);
+        } elseif (property_exists($this->message, $property)) {
+            // needed to support \stdClass instances
+            $this->message->{$property} = $value;
+        }
     }
 }
