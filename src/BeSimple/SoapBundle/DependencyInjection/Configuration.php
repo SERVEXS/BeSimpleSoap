03ff17128a2138a77df538cf444d60ae3b0472bb
Joshua Bours <joshua.bours@servexs.nl>
2023-11-17 10:59:28 +0100

    Fixed The controller for URI &quot;/api/AuthenticatorApi&quot; is not callable: Controller &quot;BeSimpleSoapBundle:SoapWebService:exception&quot; does neither exist as service nor as class

--- src/BeSimple/SoapBundle/DependencyInjection/Configuration.php
+++ src/BeSimple/SoapBundle/DependencyInjection/Configuration.php
@@ -12,6 +12,7 @@
 
 namespace BeSimple\SoapBundle\DependencyInjection;
 
+use BeSimple\SoapBundle\Controller\SoapWebServiceController;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -43,7 +44,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->scalarNode('exception_controller')->defaultValue('BeSimpleSoapBundle:SoapWebService:exception')->end()
+                ->scalarNode('exception_controller')->defaultValue('BeSimple\SoapBundle\Controller\SoapWebServiceController::exceptionAction')->end()
             ->end()
         ;
 
