8a719b0ada9ab7496ce7af21ad67b60d0ebb161a
Joshua Bours <joshua.bours@servexs.nl>
2025-07-30 14:51:30 +0200

    feature/symfony-6-compatibility: Fixed BeSimple\SoapBundle\Handler\ExceptionHandler::setSoapFault(): Argument #1 ($soapFault) must be of type SoapFault, string given, called in /application/vendor/servexs/soap/src/BeSimple/SoapBundle/Controller/SoapWebServiceController.php

--- src/BeSimple/SoapBundle/Handler/ExceptionHandler.php
+++ src/BeSimple/SoapBundle/Handler/ExceptionHandler.php
@@ -21,13 +21,13 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class ExceptionHandler
 {
-    protected ?\SoapFault $soapFault = null;
+    protected \SoapFault|string|null $soapFault = null;
 
     public function __construct(protected FlattenException $exception, protected $details = null)
     {
     }
 
-    public function setSoapFault(\SoapFault $soapFault): void
+    public function setSoapFault(\SoapFault|string $soapFault): void
     {
         $this->soapFault = $soapFault;
     }
